22a726d3ed0802cb3ce81db37e6e2a0383b8743e
nebula <miyamoto@brain.imi.i.u-tokyo.ac.jp>
2017-12-21 17:38:01 +0900

    get temp of each core

--- get_temp.sh
+++ get_temp.sh
@@ -2,4 +2,11 @@
 # get CPU temperature
 # this plugin needs lm-sensor package for sensors command
 
-echo -e "temp.all\t$(sensors | grep temp1 | sed 's/[\t ]\+/\t/g' | cut -f2 | sed -e 's/[^0-9.]//g')\t$(date +%s)"
+MAX=$(sensors | wc -l)
+
+for ID in $(seq 0 ${MAX}); do
+    TEMP=$(sensors | grep "Core ${ID}:"  | sed 's/[\t ]\+/\t/g' | cut -f3 | sed -e 's/[^0-9.]//g')
+    if [ -n "$TEMP" ]; then
+	echo -e "temp.${ID}\t$TEMP\t$(date +%s)"
+    fi
+done
